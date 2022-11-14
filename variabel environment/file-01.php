a1407d6f7d2cbe2ed065c0282e8187b5140ed35c
HasanKhoirul <tyangaming896@gmail.com>
2022-11-14 13:27:08 +0700

    Belajar Variabel Lokal, Global & Statis

--- variabel environment/file-01.php
+++ variabel environment/file-01.php
@@ -1,8 +1,6 @@
-<?
-// Variabel Global
+<?php
 
-$nama = 'Rendi Fadilah';
+$nama = 'Hasan Khoirul Ari Setiyawan'; // <-- variabel global
+
 echo "file-01.php <br>";
-echo "Nama : {$nama} <br><br>";
-
-?>
+echo "Nama: {$nama} <br><br>";
